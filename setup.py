c1327f9fd40e43d986376d75577ae838fde9be18
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-01-29 14:27:36 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="distwq", 
+    version="0.0.1",
+    author="Ivan Raikov",
+    author_email="ivan.g.raikov@gmail.com",
+    description="Distributed work queue operations using mpi4py.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iraikov/distwq",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    install_requires=[
+        'mpi4py', 'numpy'
+    ],
+
+)
